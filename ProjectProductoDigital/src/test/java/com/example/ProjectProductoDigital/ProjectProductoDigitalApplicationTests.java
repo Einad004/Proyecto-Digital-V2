039b867bc41f4032274b883a080c486e5a5e6706
Einad <floresrespaldizadaniela@gmail.com>
2026-03-28 01:15:30 -0500

    todo

---
+++ ProjectProductoDigital/src/test/java/com/example/ProjectProductoDigital/ProjectProductoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectProductoDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProductoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
