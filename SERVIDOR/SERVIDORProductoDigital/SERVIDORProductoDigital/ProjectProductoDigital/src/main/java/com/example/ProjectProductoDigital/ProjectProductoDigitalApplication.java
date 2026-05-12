23efb86b499ac853f73864b17468e493f3586666
Einad004 <floresrespaldizadaniela@gmail.com>
2026-05-12 13:36:11 -0400

    Add files via upload

---
+++ SERVIDOR/SERVIDORProductoDigital/SERVIDORProductoDigital/ProjectProductoDigital/src/main/java/com/example/ProjectProductoDigital/ProjectProductoDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectProductoDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProductoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectProductoDigitalApplication.class, args);
+	}
+
+}
